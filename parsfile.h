d9168d85de70c3bfda28dc4f0a0c773b95b880dc
_TRUPPER_ <117923568+NeTRUPPER@users.noreply.github.com>
2024-12-12 22:42:56 +0700

    Delete parsfile.h

--- parsfile.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <string>
-#include "insert.h"
-
-using namespace std;
-
-string constructFilePath(const string& schemeName, const string& tableName, const string& fileType, int fileNumber);
